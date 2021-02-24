f4154c4801839234fb37941a660171d95ecf2e06
fyodor <fyodor.zakharov@gmail.com>
2021-02-24 23:11:28 +0300

    Добавьте файлы проекта.

---
+++ Credit/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Credit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сумму кредита: ");
+            decimal sum = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Введите проценты по кредиту: ");
+            decimal sum = Convert.ToDecimal(Console.ReadLine());
+
+        }
+    }
+}
